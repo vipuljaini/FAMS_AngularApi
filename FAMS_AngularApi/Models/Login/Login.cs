7380d443aebe2b60db7802c6c393bafa787330a3
Vipul Jain <vipulcrystal@gmail.com>
2020-05-28 23:28:43 +0530

    Login

--- FAMS_AngularApi/Models/Login/Login.cs
+++ FAMS_AngularApi/Models/Login/Login.cs
@@ -128,7 +128,6 @@ namespace FAMS_AngularApi.Models.Login
             EmailSent emailobect = new EmailSent();
             try
             {
-
                 List<Responsevalue> dataList = new List<Responsevalue>(); List<UserDetails> dataList1 = new List<UserDetails>();
                 var Result = dbcontext.MultipleResults("[dbo].[Sp_UserLogin]").With<Responsevalue>().With<UserDetails>().Execute("@QueryType", "@EmailId", "ChkEmail", email);
                 dataList = Result.FirstOrDefault().Cast<Responsevalue>().ToList();
