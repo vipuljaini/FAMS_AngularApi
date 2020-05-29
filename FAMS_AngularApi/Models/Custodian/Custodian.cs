b00d88e7e66022a723550724bd1c2585cdfe0111
bibhuyoeki <bibhu@yoekisoft.com>
2020-05-29 12:00:33 +0530

    bibhucustoand securi

---
+++ FAMS_AngularApi/Models/Custodian/Custodian.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.Custodian
+{
+    public class Custodian
+    {
+        public Int32 Result { get; set; }
+        public Int64 SrNo { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string CustodianCode { get; set; }
+        public string CustodianName { get; set; }
+
+        public string PMSCode { get; set; }
+        public string PMSName { get; set; }
+        public string PMSAccNo { get; set; }
+    }
+}
---
+++ FAMS_AngularApi/Models/Security/Custodian.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.Security
+{
+    public class Custodian
+    {
+        public string CustodianCode { get; set; }
+        public string CustodianName { get; set; }
+    }
+}
