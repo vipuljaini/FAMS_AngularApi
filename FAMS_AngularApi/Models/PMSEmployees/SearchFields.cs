34943de14fc510ce93d0f81423d3858166d37838
Rock <kisuvisu95@gmail.com>
2020-06-04 04:48:45 -0700

    Work Search Filter in PMSEmployee

---
+++ FAMS_AngularApi/Models/PMSEmployees/SearchFields.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.PMSEmployees
+{
+    public class SearchFields
+    {
+        public string UserId { get; set; }
+        public string Result { get; set; }
+    }
+}
