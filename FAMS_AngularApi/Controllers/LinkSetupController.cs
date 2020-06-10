13884d1232434c7657aa9d7ba481f246fb6ed286
Vipul Jain <vipulcrystal@gmail.com>
2020-06-10 12:46:49 +0530

    Sidebar Links

---
+++ FAMS_AngularApi/Controllers/LinkSetupController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FAMS_AngularApi.Models.LinkSetup;
+
+
+namespace FAMS_AngularApi.Controllers
+{
+    public class LinkSetupController : ApiController
+    {
+        DataAccessLayer ObjDAL = new DataAccessLayer();
+        [HttpPost]
+        [Route("api/LinkSetup/GetLinks")]
+        public Dictionary<string, object> getLinksUserWise([FromBody] JsonLink LinkData)
+        {
+            return ObjDAL.GetLinks(LinkData);
+        }
+    }
+}
---
+++ FAMS_AngularApi/Models/LinkSetup/JsonLink.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.LinkSetup
+{
+    public class JsonLink
+    {
+        public string UserType { get; set; }
+    }
+}
