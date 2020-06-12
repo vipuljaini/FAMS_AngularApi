c793190cd57c8b5a26f615374f369620790f7d59
Rock <kisuvisu95@gmail.com>
2020-06-12 10:43:46 -0700

    changes after testing

---
+++ FAMS_AngularApi/Models/LinkSetup/LinkAllFields.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.LinkSetup
+{
+    public class LinkAllFields
+    {
+        public Int64 LinkID { get; set; }
+        public string LinkName { get; set; }
+        public string IconName { get; set; }
+        public string url { get; set; }
+        public Int32 ParetmenuID { get; set; }
+    }
+}
--- FAMS_AngularApi/Models/PMSEmployees/BindAllCustomersFields.cs
+++ FAMS_AngularApi/Models/PMSEmployees/BindAllCustomersFields.cs
@@ -7,12 +7,12 @@ namespace FAMS_AngularApi.Models.PMSEmployees
 {
     public class BindAllCustomersFields
     {
+        public Nullable<Int64> CustId { get; set; }
         public Nullable<Int64> srNo { get; set;}
         public string CustomerName { get; set; }
         public string CustomerCode { get; set;}
-        public Nullable<DateTime> EmpLinkingDate {get; set;}
-        public Nullable<DateTime> InceptionDate { get; set;}
-        public string Custodian { get; set;}
-        public Nullable<Int64> CustId { get; set;}
+        public string EmpLinkingDate {get; set;}
+        public string InceptionDate { get; set;}
+        public string Custodian { get; set;}    
     }
 }
