9f3b6afd0627f166ba68f905183d0cd3d74d6c3d
Vipul Jain <vipulcrystal@gmail.com>
2020-06-14 11:56:59 +0530

    Final Changes

--- FAMS_AngularApi/Models/PMSEmployees/BindAllFields.cs
+++ FAMS_AngularApi/Models/PMSEmployees/BindAllFields.cs
@@ -8,12 +8,13 @@ namespace FAMS_AngularApi.Models.PMSEmployees
     public class BindAllFields
     {
         public Nullable<Int64> PMSEmpId { get; set; }
+        public Nullable<Int64> srNo { get; set; }
         public string EmployeeCode { get; set; }
         public string EmployeeName { get; set; }
         public string Gender { get; set; }
         public string Qualification { get; set; }
         public string About { get; set; }
-        public Nullable<Int64> srNo { get; set; }
+   
         //public string CustId { get; set; }
     }
 }
