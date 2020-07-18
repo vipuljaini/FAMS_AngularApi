16c9afb169d01fc3bb1f786df9fb9497e7ae5022
Rishabh <rishabh@yoekisoft.com>
2020-07-18 09:46:06 +0530

    Rishabh

---
+++ FAMS_AngularApi/Controllers/CapitalStatementController.cs
@@ -0,0 +1,30 @@
+﻿using FAMS_AngularApi.Models.CapitalStatement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FAMS_AngularApi.Controllers
+{
+    public class CapitalStatementController : ApiController
+    {
+        CapitalStatementDAL Obj = new CapitalStatementDAL();
+        [HttpPost]
+        [Route("api/CapitalStatement/BindGrid/")]
+        public Dictionary<string, object> BindGrid(GridFields Data)
+        {
+            return Obj.BindGrid(Data);
+        }
+
+        [HttpGet]
+        [Route("api/CapitalStatement/DownloadExcel/")]
+        public Dictionary<string, object> DownloadExcel()
+        {
+            return Obj.DownloadExcel();
+        }
+
+
+    }
+}
