4003cd74f1be3085cb0fe42866a0c02916f6bbd2
Vipul Jain <vipulcrystal@gmail.com>
2020-07-27 16:39:56 +0530

    Rohit chnges

---
+++ FAMS_AngularApi/Models/NotesMaster/AllEmail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.NotesMaster
+{
+    public class AllEmail
+    {
+        public string EmailId { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ FAMS_AngularApi/Models/NotesMaster/AllNotification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.NotesMaster
+{
+    public class AllNotification
+    {
+        public Int64 NMId { get; set; }
+        public string Subject { get; set; }
+        public string Note { get; set; }
+        public string AttachmentFile { get; set; }
+        public bool IsRead { get; set; }
+        public string CreatedOn { get; set; }
+        public string CreatedOnTime { get; set; }
+        public Int32 NoteLength { get; set; }
+        public Int32 sublength { get; set; }
+        
+
+    }
+}
