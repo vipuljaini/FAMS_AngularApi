831e411ecbd1ad507b5253154d84d6f8cdaffa5e
Rock <kisuvisu95@gmail.com>
2020-07-29 08:25:02 -0700

    Work on Current por

--- FAMS_AngularApi/Models/CurrentPortfolio/GridTotalFieldData.cs
+++ FAMS_AngularApi/Models/CurrentPortfolio/GridTotalFieldData.cs
@@ -8,11 +8,11 @@ namespace FAMS_AngularApi.Models.CurrentPortfolio
     public class GridTotalFieldData
     {
         public string Security { get; set; }
-        public decimal SumTotalCost { get; set; }
-        public decimal SumMarketValue { get; set; }
-        public decimal SumIncome { get; set; }
-        public decimal SumGL { get; set; }
-        public decimal SumPercentG_L { get; set; }
-        public decimal SumPercentAssets { get; set; }
+        public Nullable<decimal> SumTotalCost { get; set; }
+        public Nullable<decimal> SumMarketValue { get; set; }
+        public Nullable<decimal> SumIncome { get; set; }
+        public Nullable<decimal> SumGL { get; set; }
+        public Nullable<decimal> SumPercentG_L { get; set; }
+        public Nullable<decimal> SumPercentAssets { get; set; }
     }
 }
