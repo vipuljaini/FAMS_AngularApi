dfeb8f92ccef473101834797a894b39b6b2fb21e
Vipul Jain <vipulcrystal@gmail.com>
2020-08-04 12:15:16 +0530

    Portfolio Chnges

---
+++ FAMS_AngularApi/Controllers/PortfolioAppraisalsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FAMS_AngularApi.Models.StateOfExpenses;
+
+
+namespace FAMS_AngularApi.Controllers
+{
+    public class PortfolioAppraisalsController : ApiController
+    {
+        DataAccessLayer ObjDAL = new DataAccessLayer();
+
+
+        [HttpGet]
+        [Route("api/PortfolioAppraisals/BindCustomer/{EmployeeId}")]
+        public Dictionary<string, object> BindCustomer(string EmployeeId)
+        {
+            return ObjDAL.BindCustomer(EmployeeId);
+        }
+
+        [HttpPost]
+        [Route("api/PortfolioAppraisals/BindEmployees/{UserId}")]
+        public Dictionary<string, object> BindEmployeesApi(string UserId)
+        {
+            return ObjDAL.BindEmployees(UserId);
+        }
+        [HttpGet]
+        [Route("api/PortfolioAppraisals/NextRecordBind/{CustomerAccount}/{FromDate}/{ToDate}/{SeqNo}")]
+        public Dictionary<string, object> NextRecordBind(string CustomerAccount, string FromDate, string ToDate, string SeqNo)
+        {
+            return ObjDAL.NextRecordBind(CustomerAccount, FromDate, ToDate, SeqNo);
+        }
+
+        [HttpGet]
+        [Route("api/PortfolioAppraisals/BindGridAllFields/{CustomerAccount}/{FromDate}/{ToDate}/{SeqNo}")]
+        public Dictionary<string, object> BindGridApi(string CustomerAccount, string FromDate, string ToDate, string SeqNo)
+        {
+            return ObjDAL.BindGrid(CustomerAccount, FromDate, ToDate, SeqNo);
+        }
+
+        [HttpGet]
+        [Route("api/PortfolioAppraisals/BindDefaultData/{CustomerAccount}/{GUserId}")]
+        public Dictionary<string, object> BindDefaultData(string CustomerAccount, string GUserId)
+        {
+            return ObjDAL.BindDefaultData(CustomerAccount, GUserId);
+        }
+    }
+}
---
+++ FAMS_AngularApi/Models/PortfolioAppraisals/PortfolioAppraisal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.PortfolioAppraisals
+{
+   
+    public class PortfolioAppraisal
+    {
+        public string Date { get; set; }
+        public string SettlementDate { get; set; }
+        public string TranRef { get; set; }
+        public string Detail { get; set; }
+        public string DescNotes { get; set; }
+        public Nullable<decimal> Amount { get; set; }
+    }
+    public class StatementOfExpenses1
+    {
+        public string Date { get; set; }
+        public string SettlementDate { get; set; }
+        public string TranRef { get; set; }
+        public string Detail { get; set; }
+        public string DescNotes { get; set; }
+        public Nullable<decimal> Amount { get; set; }
+    }
+    public class StatementOfExpenses2
+    {
+        public string Date { get; set; }
+        public string SettlementDate { get; set; }
+        public string TranRef { get; set; }
+        public string Detail { get; set; }
+        public string DescNotes { get; set; }
+        public string Amount { get; set; }
+    }
+    public class StatementOfExpenses3 // fOR HEADING IN CENTER
+    {
+        public string Date { get; set; }
+    }
+
+    public class StatementOfExpenses4 // fOR grid body without header
+    {
+
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string CustomerCode { get; set; }
+        public string Date { get; set; }
+        public string SettlementDate { get; set; }
+        public string TranRef { get; set; }
+        public string Detail { get; set; }
+        public string DescNotes { get; set; }
+        public string Amount { get; set; }
+    }
+
+    public class StatementOfExpenses_Default
+    {
+        public string CustomerCode { get; set; }
+        public string CustomerAccount { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+    }
+    public class StatementOfExpenses5
+    {
+        public string TransactionDescription { get; set; }
+        public string TotalPaidAmount { get; set; }
+        public string TotalPayableAmount { get; set; }
+
+    }
+    public class BindEmployees
+    {
+        public Nullable<Int64> PMSEmpId { get; set; }
+        public string EmployeeName { get; set; }
+    }
+
+    public class Customer
+    {
+        public Int64 UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
