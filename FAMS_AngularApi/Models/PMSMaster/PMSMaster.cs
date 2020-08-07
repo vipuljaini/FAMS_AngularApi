79287da0ee1caccb14ecc790e4a710e2cc8f354e
bibhuyoeki <bibhu@yoekisoft.com>
2020-08-07 21:35:39 +0530

    2MasterDone_WithSchemeMasterPMSMaster_07Aug2020_EOD

---
+++ FAMS_AngularApi/Models/PMSMaster/PMSMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAMS_AngularApi.Models.PMSMaster
+{
+    public class PMSMaster
+    {
+        public Int64 PMSId { get; set; }
+        public string PMSCode { get; set; }
+        public string PMSName { get; set; }
+        public string PMSAccountNumber { get; set; }
+        public Int32 Result { get; set; }
+        public Int64 SrNo { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedOn { get; set; }
+    }
+}
